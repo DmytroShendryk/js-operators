9aa9a7e01059cde5058794fc3134638f0b0ddef2
DmytroShendryk <dimoon674@gmail.com>
2023-01-05 15:22:01 +0200

    operator-fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const numFirst = +prompt ("enter number");
-const operator = prompt("enter operator"); // any: +, -, *, /, %
 const numSecond = +prompt ("enter number");
-alert ("user entered" + (numFirst + operator + numSecond))
+alert (`user entered ${numFirst} and ${numSecond}`);
 let addition = numFirst + numSecond;
 let subtraction = numFirst - numSecond;
 let multiplication = numFirst * numSecond;
